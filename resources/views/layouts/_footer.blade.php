cd70d197cd8fb0acb3af54e1d7fbb1022a5c7e16
anna <1085177899@qq.com>
2018-11-01 14:13:10 +0800

    The begin

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+	<div class="container">
+		<p class="pull-left">
+			haliluya
+		</p>
+		<p class="pull-right">
+			<a href="mailto:anna.zhong@qq.com">联系我们</a>
+		</p>
+	</div>
+</footer>
---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar navbar-default navbar-static-top">
+	<div class="container">
+		<div class="nav-header">
+			<button type="button" class="navbar-toggle collapsed">
+				<span class="sr-only"> Toggle Navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="{{ url('/') }}"> Laravel house</a>
+		</div>
+		<div class="collapse nav-collapse" id="app-navbar-collapse">
+			<ul class="nav navbar-nav"></ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">登录</a></li>
+				<li><a href="#">注册</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
