570881ca98f25f8663e1ecafa426af0a16be808c
anna <1085177899@qq.com>
2018-11-03 14:13:17 +0800

    add overtrue/laravel-lang

--- app/Http/Controllers/UserAddressesController.php
+++ app/Http/Controllers/UserAddressesController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Models\UserAddress;
 
+use App\Http\Requests\UserAddressRequest;
+
 class UserAddressesController extends Controller
 {
     public function index(Request $request)
@@ -18,5 +20,20 @@ class UserAddressesController extends Controller
     public function create()
     {
         return view('user_addresses.create_and_edit', ['address' => new UserAddress()]);
+    }
+
+    public function store(UserAddressRequest $request)
+    {
+        $request->user()->addresses()->create($request->only([
+            'province',
+            'city',
+            'district',
+            'address',
+            'zip',
+            'contact_name',
+            'contact_phone',
+        ]));
+
+        return redirect()->route('user_addresses.index');
     }
 }
